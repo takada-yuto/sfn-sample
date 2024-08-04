73c2b2b1b39e97f0db3351a9e44b21abcce59fdd
takada-yuto <yuto1999.0516@gmail.com>
2024-08-04 20:42:53 +0900

    フロント構築

--- lib/sfn-sample-stack.ts
+++ lib/sfn-sample-stack.ts
@@ -1,16 +1,69 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib"
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+} from "aws-cdk-lib/aws-cloudfront"
+import { AnyPrincipal, Effect, PolicyStatement } from "aws-cdk-lib/aws-iam"
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3"
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment"
+import { Construct } from "constructs"
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SfnSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const frontendBucket = new Bucket(this, "SfnSampleFrontendBucket", {
+      websiteIndexDocument: "index.html",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SfnSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const policyStatement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:GetObject"],
+      principals: [new AnyPrincipal()],
+      resources: [frontendBucket.arnForObjects("*")],
+    })
+    frontendBucket.addToResourcePolicy(policyStatement)
+
+    const sfnSampleOAI = new OriginAccessIdentity(this, "SfnSampleFrontendOAI")
+    frontendBucket.grantRead(sfnSampleOAI)
+
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      "SfnSampleFrontendWebDestribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: frontendBucket,
+              originAccessIdentity: sfnSampleOAI,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    )
+
+    const cloudfrontUrl = `https://${distribution.distributionDomainName}`
+
+    // CloudFrontディストリビューションのドメイン名を出力
+    new cdk.CfnOutput(this, "cloudfrontUrl", {
+      value: cloudfrontUrl,
+    })
+
+    new BucketDeployment(this, "SfnSampleFrontendBucketDeployment", {
+      sources: [Source.asset("frontend/out")],
+      destinationBucket: frontendBucket,
+      distribution: distribution,
+      distributionPaths: ["/*"],
+    })
   }
 }
